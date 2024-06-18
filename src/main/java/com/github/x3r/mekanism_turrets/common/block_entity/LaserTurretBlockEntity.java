136d8bec91287dc955f002fce3b586a422b537ae
X3RMINATION <nicolaeungur@gmail.com>
2024-06-18 10:36:45 -0700

    fix turret targeting players when it shouldnt, now doesnt target miscellaneous targets

--- src/main/java/com/github/x3r/mekanism_turrets/common/block_entity/LaserTurretBlockEntity.java
+++ src/main/java/com/github/x3r/mekanism_turrets/common/block_entity/LaserTurretBlockEntity.java
@@ -5,6 +5,7 @@ import com.github.x3r.mekanism_turrets.MekanismTurretsConfig;
 import com.github.x3r.mekanism_turrets.common.entity.LaserEntity;
 import com.github.x3r.mekanism_turrets.common.registry.SoundRegistry;
 import com.github.x3r.mekanism_turrets.common.scheduler.Scheduler;
+import jdk.jfr.Category;
 import mekanism.api.*;
 import mekanism.api.math.FloatingLong;
 import mekanism.api.providers.IBlockProvider;
@@ -28,6 +29,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.player.Player;
@@ -224,10 +226,11 @@ public class LaserTurretBlockEntity extends TileEntityMekanism implements GeoBlo
     }
 
     private boolean turretFlagsAllowTargeting(LivingEntity e) {
-        if(this.targetsHostile && e.getType().getCategory().equals(MobCategory.MONSTER)) {
+        MobCategory category = e.getType().getCategory();
+        if(this.targetsHostile && !category.isFriendly()) {
             return true;
         }
-        if(this.targetsPassive && !e.getType().getCategory().equals(MobCategory.MONSTER)) {
+        if(this.targetsPassive && category.isFriendly() && !category.equals(MobCategory.MISC)) {
             return true;
         }
         UUID owner = SecurityUtils.get().getOwnerUUID(this);
