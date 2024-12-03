aa45ba7ab8db2710cc1d752a71825fd895c311d2
- S i l v e t - <s1178484362@163.com>
2024-12-03 18:45:52 +0800

    massive code fix - bypasses_effects.json : make laser&fence damage bypassed effects - DamageTypeRegistry.java : adding an overwrite which contains causingEntity to achieve dropping exp + items which is normally dropping while player kill the mobs - LaserEntity.java : make laser damage cannot bypass players' armor+protection enchantment but bypass mobs'  same reason as "DamageTypeRegistry.java" changing some code to achieve that goal

--- src/main/java/com/github/x3r/mekanism_turrets/common/entity/LaserEntity.java
+++ src/main/java/com/github/x3r/mekanism_turrets/common/entity/LaserEntity.java
@@ -3,15 +3,28 @@ package com.github.x3r.mekanism_turrets.common.entity;
 import com.github.x3r.mekanism_turrets.common.block.LaserTurretBlock;
 import com.github.x3r.mekanism_turrets.common.registry.DamageTypeRegistry;
 import com.github.x3r.mekanism_turrets.common.registry.EntityRegistry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import org.checkerframework.checker.units.qual.A;
+import software.bernie.geckolib.event.GeoRenderEvent;
+import net.minecraft.world.entity.player.Player;
+
+import static net.minecraft.world.item.enchantment.EnchantmentHelper.getEnchantmentLevel;
 
 public class LaserEntity extends Projectile {
 
@@ -36,8 +49,28 @@ public class LaserEntity extends Projectile {
             if(hitResult.getType().equals(HitResult.Type.BLOCK)) {
                 onHitBlock((BlockHitResult) hitResult);
             }
-            level().getEntities(this, getBoundingBox().inflate(0.5)).forEach(entity -> {
-                entity.hurt(new DamageTypeRegistry(level().registryAccess()).laser(), (float) this.damage);
+            level().getEntities(this, getBoundingBox().inflate(0.75)).forEach(entity -> {
+                boolean isPlayer = entity.getType().toString().equals("entity.minecraft.player");
+                if(isPlayer){
+                    float armor = 0;
+                    float toughness = 0;
+                    float protectEnchantReduction = (float) (0.01*Math.max(80,4*getEnchantmentLevel(Enchantments.ALL_DAMAGE_PROTECTION, (LivingEntity) entity)));
+                    for (ItemStack armorStack : entity.getArmorSlots()) {
+                        if (armorStack.getItem() instanceof ArmorItem armorItem) {
+                            armor = armor + armorItem.getDefense();
+                            toughness = toughness + armorItem.getToughness();
+                        }
+                    }
+                    float damageReductionRate = (float) (Math.min(20,Math.max(armor/5,armor - ((4*this.damage)/(Math.min(toughness,20)+8)) ))/25);
+                    this.damage = this.damage * (1-damageReductionRate) * (1-protectEnchantReduction);
+                }
+                Entity player = getOwner();
+                try {
+                    ((LivingEntity) entity).setLastHurtByPlayer((Player) this.getOwner());
+                } catch (Exception e) {
+                    ;
+                }
+                entity.hurt(new DamageTypeRegistry(level().registryAccess(),player).laser(), (float) this.damage);
             });
             lifeTime++;
             if (lifeTime > 10 * 20) {
--- src/main/java/com/github/x3r/mekanism_turrets/common/registry/DamageTypeRegistry.java
+++ src/main/java/com/github/x3r/mekanism_turrets/common/registry/DamageTypeRegistry.java
@@ -8,6 +8,9 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.entity.Entity;
+
+import javax.annotation.Nullable;
 
 public class DamageTypeRegistry {
 
@@ -20,10 +23,19 @@ public class DamageTypeRegistry {
         this.electricFence = source(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(MekanismTurrets.MOD_ID, "electric_fence")));
         this.laser = source(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(MekanismTurrets.MOD_ID, "laser")));
     }
+    public DamageTypeRegistry(RegistryAccess registryAccess,Entity pCausingEntity) {
+        this.damageTypes = registryAccess.registryOrThrow(Registries.DAMAGE_TYPE);
+        this.electricFence = source(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(MekanismTurrets.MOD_ID, "electric_fence")),pCausingEntity);
+        this.laser = source(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(MekanismTurrets.MOD_ID, "laser")),pCausingEntity);
+    }
 
     private DamageSource source(ResourceKey<DamageType> pDamageTypeKey) {
         return new DamageSource(this.damageTypes.getHolderOrThrow(pDamageTypeKey));
     }
+    private DamageSource source(ResourceKey<DamageType> pDamageTypeKey, Entity pCausingEntity){
+        return new DamageSource(this.damageTypes.getHolderOrThrow(pDamageTypeKey),pCausingEntity);
+    }
+
 
     public DamageSource electricFence() {
         return this.electricFence;
